127986a6b098c42ea392bde5aa2feb1e8ca39774
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-08-18 19:34:14 -0600

    Done

--- director.R
+++ director.R
@@ -3,10 +3,25 @@
 db<-read.csv('DatosCrudos.csv')
 View(db)
 
+db[db$DIRECTOR == "-----------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
+db[db$DIRECTOR == "-----------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
+db[db$DIRECTOR == "-------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
+db[db$DIRECTOR == "----" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
+db[db$DIRECTOR == "------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
 db[db$DIRECTOR == "---" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 
 db[db$DIRECTOR == "--" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 
+db[db$DIRECTOR == "-" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
 db[db$DIRECTOR == "." & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+
+
+-----------------
 
 View(db)
