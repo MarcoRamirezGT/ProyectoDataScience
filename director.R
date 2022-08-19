8506af861efd02ee3773d875cb585b9a2bf400e1
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-08-18 19:49:39 -0600

    Done 3.0

--- director.R
+++ director.R
@@ -1,26 +1,42 @@
 #Reparando el director
 
 db<-read.csv('DatosCrudos.csv')
-View(db)
 
-db[db$DIRECTOR == "-----------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
-db[db$DIRECTOR == "-----------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
-db[db$DIRECTOR == "-------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
-db[db$DIRECTOR == "----" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
-db[db$DIRECTOR == "------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
-db[db$DIRECTOR == "---" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
-db[db$DIRECTOR == "--" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 
 db[db$DIRECTOR == "-" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
+db[db$DIRECTOR == "--" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "---" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "----" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-----" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "--------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "---------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "----------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-----------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "--------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "---------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "----------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-----------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "--------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "---------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "----------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-----------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "--------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "---------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "----------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "-----------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "------------------------------" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA                   
+db[db$DIRECTOR == ' "" ' & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == " ' " & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == ' " ' & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 db[db$DIRECTOR == "." & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
-
 db[db$DIRECTOR == "0" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 
 
