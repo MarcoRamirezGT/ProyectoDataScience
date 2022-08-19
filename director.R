b3778d9624c86079d150fb3bd8eeeafa689e688f
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-08-18 20:04:53 -0600

    Done 300

--- director.R
+++ director.R
@@ -36,6 +36,8 @@ db[db$DIRECTOR == "------------------------------" & !is.na(db$DIRECTOR), ]$DIRE
 db[db$DIRECTOR == ' "" ' & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 db[db$DIRECTOR == " ' " & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 db[db$DIRECTOR == ' " ' & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == "..." & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
+db[db$DIRECTOR == ".." & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 db[db$DIRECTOR == "." & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 db[db$DIRECTOR == "0" & !is.na(db$DIRECTOR), ]$DIRECTOR <- NA
 
